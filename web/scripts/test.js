72048bbc8e806177fdab8ab1e2454ea4e32d108d
Marianna <penamarianna@gmail.com>
2014-03-24 17:28:52 -0600

    removed unused files

--- web/scripts/test.js
+++
@@ -1,5 +0,0 @@
-var abstractionObj = {
-	"abstractionURI": "http://openvisko.org/rdf/ontology/visko-view.owl#1D_Timeline",
-	"abstractionName": "1D_Timeline"
-};
-document.getElementById("abs1").innerHTML = abstractionObj.abstractionName;
