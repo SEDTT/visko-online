c25227817a483fcdba7c6326e9c8ceaa2cc7fb54
Alex Knaust <awknaust@gmail.com>
2014-03-25 19:38:30 -0600

    Added SQL to push initial ViewerSets into DB

---
+++ web/initial_viewersets.sql
@@ -0,0 +1,5 @@
+INSERT INTO ViewerSets (URI)
+ VALUES
+('http://visko.cybershare.utep.edu:5080/visko-web/registry/module_d3.owl#d3-viewer-set'),
+('http://visko.cybershare.utep.edu:5080/visko-web/registry/module_paraview.owl#paraview'),
+('http://visko.cybershare.utep.edu:5080/visko-web/registry/module_webbrowser.owl#web-browser');
