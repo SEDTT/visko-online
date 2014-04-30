e0c3266ccec00a70e516dc2dfa77b4c5bec87bc0
Marianna <penamarianna@gmail.com>
2014-04-30 17:06:05 -0600

    viskoquery

--- web/source/viskoapi/ViskoQuery.php
+++ web/source/viskoapi/ViskoQuery.php
@@ -1,33 +1,113 @@
 <?php
-require_once 'JsonTransformer.php';
 require_once 'JsonCerializable.php';
+require_once 'JsonDeserializable.php';
 
-class ViskoQuery implements JsonCerializable{
+class ViskoQuery implements JsonCerializable, JsonDeserializable{
 	private $vsql;
 	private $targetFormatURI;
 	private $targetTypeURI;
 	private $viewURI;
 	private $viewerSetURI;
 	private $artifactURL;
+	private $parameterBindings;
+	private $formatURI;
+	private $typeURI;
 	
-	public function __construct($queryText){
-		$this->setQueryText($queryText);
+	public function __construct(){
+		
+	}
+	
+	public function init($vsql, $formatURI, $typeURI, $targetFormatURI, $targetTypeURI, $viewURI, $viewerSetURI, $artifactURL, $parameterBindings){
+		$this->setQueryText($vsql);
+		$this->formatURI = $formatURI;
+		$this->typeURI = $typeURI;
+		$this->targetFormatURI = $targetFormatURI;
+		$this->targetTypeURI = $targetTypeURI;
+		$this->viewURI = $viewURI;
+		$this->viewerSetURI = $viewerSetURI;
+		$this->artifactURL = $artifactURL;
+		$this->parameterBindings = $parameterBindings;
 	}
 	
 	public function getQueryText(){
 		return $this->vsql;
 	}
 	
-	public function setQueryText($queryText){
-		$this->vsql = $queryText;
+	public function getFormatURI(){
+		return $this->formatURI;
 	}
 	
+	public function getTypeURI(){
+		return $this->typeURI;
+	}
+	
+	public function getArtifactURL(){
+		return $this->artifactURL;
+	}
+	
+	public function getTargetFormatURI(){
+		return $this->targetFormatURI;
+	}
+	
+	public function getTargetTypeURI(){
+		return $this->targetTypeURI;
+	}
+	
+	public function getViewURI(){
+		return $this->viewURI;
+	}
+	
+	public function getViewerSetURI(){
+		return $this->viewerSetURI;
+	}
+	
+	public function setQueryText($queryText){
+		$this->vsql = trim($queryText);
+	}
+	
+	public function getParameterBindings(){
+		return $this->parameterBindings;
+	}
+
+	/**
+	* When convertig to JSON, simply write text version of self?.
+	* TODO change this to work with parameters?
+	*
+	*/
 	public function toJson(){
-		$jt = new JsonTransformer;
+		if($this->vsql != null)
+		{
 		$attrs = array(
 			"type" => "Query",
-			"vsql" => $this->vsql
+			"vsql" => $this->vsql);
+		}
+		else
+		{		
+		$attrs = array(
+			"type" => "Query",
+			"formatURI" => $this->formatURI,
+			"typeURI" => $this ->typeURI,
+			"artifactURL" => $this->artifactURL,
+			"targetTypeURI"=> $this->targetTypeURI,
+			"targetFormatURI"=> $this->targetFormatURI,
+			"viewURI" => $this->viewURI,
+			"viewerSetURI" => $this->viewerSetURI	
 		);
-		return $jt->encode($attrs);
+		}
+		return $attrs;
+	}
+	
+	public function fromJson($json){
+		$this->vsql = $json->vsql;
+		$this->formatURI = $json->formatURI;
+		$this->typeURI = $json->typeURI;	
+		$this->targetFormatURI = $json->targetFormatURI;
+		$this->targetTypeURI = $json->targetTypeURI;
+		$this->viewURI = $json->viewURI;
+		$this->viewerSetURI = $json->viewerSetURI;
+		$this->artifactURL = $json->artifactURL;
+
+		//weird php hack to get object fields as assoc array
+		$this->parameterBindings = get_object_vars($json->parameterBindings);	
 	}
 }
